b341d8513534176b9f12b1b2d0a75cfe48007ab4
User <2176589@mail.ru>
2021-07-28 23:39:12 -0700

    Add: confirm delete message

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,6 +4,7 @@ import { ActivityItem, Icon } from '@fluentui/react';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import { DefaultButton } from '@fluentui/react/lib/Button';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import { getPosts } from '../../actions/posts';
 import { deletePost } from '../../redusers/postsReduser';
 import './Posts.css';
@@ -13,16 +14,16 @@ const Posts = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.items);
   // const [currentPage, setCurrentPage] = useState(1);
-  const [fetching, setFetching] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
+  const [postId, setPostId] = useState('');
 
   useEffect(() => {
-    if (fetching) {
-      dispatch(getPosts());
-    }
-  }, [dispatch, fetching]);
+    dispatch(getPosts());
+    // setCurrentPage((prevState) => prevState + 1);
+  }, [dispatch]);
   const scrollHandler = (event) => {
     if (event.target.documentElement.scrollHeight - (event.target.documentElement.scrollTop + window.innerHeight) < 100) {
-      setFetching(true);
+      console.log('');
     }
   };
   useEffect(() => {
@@ -34,6 +35,37 @@ const Posts = () => {
 
   return (
     <>
+      <CSSTransition
+        in={showMessage}
+        timeout={300}
+        classNames="alert"
+        unmountOnExit
+      >
+        <div className="alert-container">
+          <p className="alert-message">
+            Are you sure you want to delete the message?
+          </p>
+          <div className="buttons-container">
+            <button
+              type="button"
+              onClick={() => setShowMessage(false)}
+              className="cancel-button"
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setShowMessage(false);
+                dispatch(deletePost(postId));
+              }}
+              className="danger-button"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </CSSTransition>
       {posts.map((post) => (
         <div className="post-container" key={post.id}>
           <ActivityItem
@@ -50,7 +82,10 @@ const Posts = () => {
           <DefaultButton
             className="delete-button"
             aria-hidden="true"
-            onClick={() => dispatch(deletePost(post.id))}
+            onClick={() => {
+              setShowMessage(true);
+              setPostId(post.id);
+            }}
             allowDisabledFocus
             text={<Icon iconName="Trash" />}
           />
